28df33a898c085c829aad116e5e0c8819b5d7662
lukerm <13883308+lukerm@users.noreply.github.com>
2024-11-06 13:22:59 +0000

    employ batch norm inside the double convolution

--- src/train/train_utils.py
+++ src/train/train_utils.py
@@ -64,8 +64,10 @@ class UNet(nn.Module):
         # Double convolutional layers followed by ReLU activation
         return nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
         )
 

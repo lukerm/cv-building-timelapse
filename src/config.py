55794f8216f4131dd9a5b66b3c5548f6628acdaa
lukerm <13883308+lukerm@users.noreply.github.com>
2024-11-11 08:38:59 +0000

    basic config for predictions

---
+++ src/config.py
@@ -0,0 +1,20 @@
+import os
+
+PHOTO_ROOT = os.path.expanduser('~/Pictures/London/.../balcony/')
+HI_RES_FOLDER = os.path.join(PHOTO_ROOT, 'construction')
+LO_RES_FOLDER = os.path.join(PHOTO_ROOT, 'construction_lores')
+
+CROP_SIZE = (256, 256)
+
+MODEL_DIR = os.path.expanduser('~/cv-building-timelapse/models/experiments/256/')
+MODEL_MAP = {
+    'D2': os.path.join(MODEL_DIR, '025_gpu', 'e88.model.weights'),
+    'L2': os.path.join(MODEL_DIR, '015_gpu', 'best.model.weights'),
+    'R1': os.path.join(MODEL_DIR, '004_gpu', 'best.model.weights'),
+    'R3': os.path.join(MODEL_DIR, '035_gpu', 'best.model.weights'),
+}
+
+
+
+
+

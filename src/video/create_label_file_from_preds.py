b8ca4fec5a75363a99cb343562e512b26d06e639
lukerm <13883308+lukerm@users.noreply.github.com>
2024-11-15 22:12:37 +0000

    refactor label creation to accommodate multi-output predictions; include confidence probabilities

--- src/video/create_label_file_from_preds.py
+++ src/video/create_label_file_from_preds.py
@@ -6,7 +6,7 @@ to Label Studio exports. These can be read by adjust_imgs.py and potentially Lab
 import json
 import os
 from datetime import date
-from typing import List
+from typing import Dict
 
 import pandas as pd
 
@@ -14,36 +14,39 @@ from src.utils import extract_date_from_filename
 
 PREDICTIONS_ROOT = '/home/luke/cv-building-timelapse/data/predictions/'
 PREDICTIONS_PATH_MAP = {
-    'R1': 'R1/004_gpu/coords/',
-    'D2': 'D2/025_gpu/coords/',
+    'R_group': 'R_group/507_gpu/coords/',
+    'DL_group': 'DL_group/530_gpu/coords/',
 }
 
-KEYPOINTS = ['D2', 'R1']
+DL_GROUP_KEYPOINTS = {0:'D1', 1:'D2', 2:'D3', 3:'L2'}
+R_GROUP_KEYPOINTS = {0:'R1', 1:'R2', 2:'R3', 3:'R4'}
 
 IMG_SIZE = (1024, 768)  # lo resolution
 
 
-def get_label_dataframe(keypoints: List[str] = KEYPOINTS) -> pd.DataFrame:
+def get_label_dataframe(keypoints_left: Dict[int, str], keypoints_right: Dict[int, str]) -> pd.DataFrame:
 
     label_rows = []
-    for kp in keypoints:
-        coord_files = sorted(os.listdir(os.path.join(PREDICTIONS_ROOT, PREDICTIONS_PATH_MAP[kp])))
+    for kp_group in PREDICTIONS_PATH_MAP.keys():
+        is_left = kp_group == 'DL_group'  # False means right
+        coord_files = sorted(os.listdir(os.path.join(PREDICTIONS_ROOT, PREDICTIONS_PATH_MAP[kp_group])))
         coord_files = [f for f in coord_files if f.startswith('PXL_')]
         for f in coord_files:
-            df = pd.read_csv(os.path.join(PREDICTIONS_ROOT, PREDICTIONS_PATH_MAP[kp], f))
-            if len(df) == 0:
-                continue
-            row = df.iloc[0]
-            width = 100 * row['x'] / IMG_SIZE[0]
-            height = 100 * row['y'] / IMG_SIZE[1]
-            f_jpg = f.replace('csv', 'jpg')
-            label_rows.append((f_jpg, width, height, kp))
+            df = pd.read_csv(os.path.join(PREDICTIONS_ROOT, PREDICTIONS_PATH_MAP[kp_group], f))
+            df = df.sort_values('p', ascending=False)
+            for _, row in df.iterrows():
+                f_jpg = f.replace('csv', 'jpg')
+                width = 100 * row['x'] / IMG_SIZE[0]
+                height = 100 * row['y'] / IMG_SIZE[1]
+                prob = row['p']
+                kp = keypoints_left[row['idx']] if is_left else keypoints_right[row['idx']]
+                label_rows.append((f_jpg, width, height, prob, kp, is_left))
 
-    return pd.DataFrame(label_rows, columns=['img_filename', 'x_pc', 'y_pc', 'kp'])
+    return pd.DataFrame(label_rows, columns=['img_filename', 'x_pc', 'y_pc', 'prob', 'kp', 'is_left'])
 
 
 if __name__ == "__main__":
-    df = get_label_dataframe(keypoints=KEYPOINTS)
+    df = get_label_dataframe(keypoints_left=DL_GROUP_KEYPOINTS, keypoints_right=R_GROUP_KEYPOINTS)
 
     # ensure that we only take images that appear twice
     dups = df.groupby('img_filename')['kp'].count()
@@ -59,13 +62,15 @@ if __name__ == "__main__":
     out_labels = []
     for img_filename in df_dups['img_filename'].unique():
         my_results = []
-        for kp in KEYPOINTS:
-            row = df_dups[(df_dups['img_filename'] == img_filename) & (df_dups['kp'] == kp)].iloc[0]
+        my_df = df_dups[df_dups['img_filename'] == img_filename]
+        for _, row in my_df.iterrows():
             my_results.append({
                 'value': {
                     'x': float(row['x_pc']),
                     'y': float(row['y_pc']),
+                    'p': float(row['prob']),
                     'keypointlabels': [row['kp']],
+                    'is_left': row['is_left'],
                 }
             })
 
@@ -76,7 +81,6 @@ if __name__ == "__main__":
             'predictions': [{'result': my_results,}],
         })
 
-    # out_data = [{'predictions': out_labels}]
-    save_fname = 'labels_some_errors_D2_R1.json'
+    save_fname = 'labels_pretty_good_DL_R_groups_2024-11-15.json'
     with open(os.path.join(PREDICTIONS_ROOT, save_fname), 'w') as j:
         json.dump(out_labels, j)

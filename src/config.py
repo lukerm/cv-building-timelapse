fd205063756aaddf57efa75ef12dbbe0c5040a91
lukerm <13883308+lukerm@users.noreply.github.com>
2024-11-15 22:19:29 +0000

    change the crop size to 512

--- src/config.py
+++ src/config.py
@@ -4,9 +4,9 @@ IMAGE_ROOT = os.path.expanduser('~/Pictures/London/.../balcony/')
 HI_RES_FOLDER = os.path.join(IMAGE_ROOT, 'construction')
 LO_RES_FOLDER = os.path.join(IMAGE_ROOT, 'construction_lores')
 
-CROP_SIZE = (256, 256)
+CROP_SIZE = (512, 512)
 
-MODEL_DIR = os.path.expanduser('~/cv-building-timelapse/models/experiments/256/')
+MODEL_DIR = os.path.expanduser(f'~/cv-building-timelapse/models/experiments/{CROP_SIZE[0]}/')
 MODEL_MAP = {
     'D2': os.path.join(MODEL_DIR, '025_gpu', 'e88.model.weights'),
     'L2': os.path.join(MODEL_DIR, '015_gpu', 'best.model.weights'),

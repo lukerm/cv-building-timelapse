f06dc973490eacc3ffa8cc45ece64903b8554cd5
lukerm <13883308+lukerm@users.noreply.github.com>
2024-11-15 22:19:55 +0000

    config: update MODEL_MAP

--- src/config.py
+++ src/config.py
@@ -8,10 +8,14 @@ CROP_SIZE = (512, 512)
 
 MODEL_DIR = os.path.expanduser(f'~/cv-building-timelapse/models/experiments/{CROP_SIZE[0]}/')
 MODEL_MAP = {
+    'D1': os.path.join(MODEL_DIR, '051_gpu', 'best.model.weights'),
     'D2': os.path.join(MODEL_DIR, '025_gpu', 'e88.model.weights'),
     'L2': os.path.join(MODEL_DIR, '015_gpu', 'best.model.weights'),
     'R1': os.path.join(MODEL_DIR, '004_gpu', 'best.model.weights'),
-    'R3': os.path.join(MODEL_DIR, '035_gpu', 'best.model.weights'),
+    'R3': os.path.join(MODEL_DIR, '042_gpu', 'best.model.weights'),
+    'R4': os.path.join(MODEL_DIR, '056_gpu', 'best.model.weights'),
+    'R_group': os.path.join(MODEL_DIR, '507_gpu', 'best.model.weights'),
+    'DL_group': os.path.join(MODEL_DIR, '530_gpu', 'best.model.weights'),
 }
 
 

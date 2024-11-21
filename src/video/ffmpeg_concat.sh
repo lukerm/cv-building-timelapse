12a29e7f1ba04e2d2b92dc8ab4fcbd5e38e92933
lukerm <13883308+lukerm@users.noreply.github.com>
2024-11-21 10:00:56 +0000

    config: update folders to latest version

--- src/video/ffmpeg_concat.sh
+++ src/video/ffmpeg_concat.sh
@@ -1,3 +1,3 @@
 # test videos
-cd ~/cv-building-timelapse/data/adjust_translated/v1/
+cd ~/cv-building-timelapse/data/adjust_translated_rotated/v6_preds_v3/
 ffmpeg -framerate 10 -pattern_type glob -i '*.jpg' -c:v libx264 -pix_fmt yuv420p test_vid_f10_v1.mp4
